829f2ba5ba4b974e998fa9a4148799da9dae590e
Derry <thanhhaivevo@gmail.com>
2022-03-13 17:17:26 +0700

    update store

---
+++ js/products.js
@@ -0,0 +1,119 @@
+var store = new Vue({
+    el: '#app',
+    data: {
+        sellitems: [
+            {
+                Name: "Gucci Tee Black",
+                Price: "2000",
+                ID: 11,
+            },
+            {
+                Name: "Gucci Tee Boutuque",
+                Price: "3000",
+                ID: 12,
+            },
+            {
+                Name: "Gucci Tee Flash",
+                Price: "1500",
+                ID: 13,
+            },
+            {
+                Name: "Gucci Tee Munder",
+                Price: "1670",
+                ID: 14,
+            },
+            {
+                Name: "Gucci Tee Cat",
+                Price: "1970",
+                ID: 15,
+            },
+            {
+                Name: "Gucci Tee Red",
+                Price: "1070",
+                ID: 16,
+            },
+            {
+                Name: "Gucci Tee White",
+                Price: "1670",
+                ID: 17,
+            },
+
+        ],
+        sellitems2: [
+            {
+                Name: "Gucci Sneaker Black",
+                Price: "2000",
+                ID: 1,
+            },
+            {
+                Name: "Gucci Sneaker Bunder",
+                Price: "3000",
+                ID: 2,
+            },
+            {
+                Name: "Gucci Sneaker Flash",
+                Price: "1500",
+                ID: 3,
+            },
+            {
+                Name: "Gucci Sneaker Munder",
+                Price: "1670",
+                ID: 4,
+            },
+            {
+                Name: "Gucci Sneaker Mickey",
+                Price: "1670",
+                ID: 5,
+            },
+            {
+                Name: "Gucci Tee Thung",
+                Price: "1670",
+                ID: 6,
+            },
+            {
+                Name: "Gucci Tee Orgensa",
+                Price: "1670",
+                ID: 7,
+            },
+
+        ],
+        sellitems3: [
+            {
+                Name: "Gucci Sneaker Black",
+                Price: "2000",
+                ID: 1,
+            },
+            {
+                Name: "Gucci Bag Bunder",
+                Price: "3000",
+                ID: 2,
+            },
+            {
+                Name: "Gucci Bag Flash",
+                Price: "1500",
+                ID: 3,
+            },
+            {
+                Name: "Gucci Bag Munder",
+                Price: "1670",
+                ID: 4,
+            },
+            {
+                Name: "Gucci Bag Mickey",
+                Price: "1670",
+                ID: 5,
+            },
+            {
+                Name: "Gucci Bag Thung",
+                Price: "1670",
+                ID: 6,
+            },
+            {
+                Name: "Gucci Bag Orgensa",
+                Price: "1670",
+                ID: 7,
+            },
+
+        ],
+    },
+})
